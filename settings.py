f088e47436ea72f49b0eb7bf940846a2c4119101
Sven Grunewaldt <strayer@olle-orks.org>
2013-02-22 20:52:39 +0100

    Whitespaces

--- settings.py
+++ settings.py
@@ -3,13 +3,16 @@ import os
 NGINX_CWD = os.path.join("C:\\", "opt", "nginx")
 NGINX_EXECUTABLE = os.path.join(NGINX_CWD, "nginx.exe")
 NGINX_CONF = os.path.join(NGINX_CWD, "conf", "nginx.conf")
+
 PHP_CWD = os.path.join("C:\\", "opt", "php-5.4")
 PHP_EXECUTABLE = os.path.join(PHP_CWD, "php-cgi.exe")
 PHP_INI = os.path.join(PHP_CWD, "php.ini")
+
 MONGODB_CWD = os.path.join("E:\\", "bin", "srv", "mongodb")
 MONGODB_EXECUTABLE = os.path.join(MONGODB_CWD, "bin", "mongod.exe")
 MONGODB_CLIENT_EXECUTABLE = os.path.join(MONGODB_CWD, "bin", "mongo.exe")
 MONGODB_CONF = os.path.join(MONGODB_CWD, "mongodb.conf")
+
 MYSQL_CWD = os.path.join("C:\\", "opt", "mysql")
 MYSQLD_EXECUTABLE = os.path.join(MYSQL_CWD, "bin", "mysqld.exe")
 MYSQLADMIN_EXECUTABLE = os.path.join(MYSQL_CWD, "bin", "mysqladmin.exe")
